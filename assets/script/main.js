f18b70e193b6e4416e4e51eb27faf3a73e021177
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2022-12-12 12:52:32 +0100

    1 tot en met 5

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,68 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+let interaction = document.querySelector("a:nth-of-type(12)");
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+interaction.addEventListener("click", jumpHandler);
+interaction.addEventListener("animationend", jumpHandler);
 
 function jumpHandler() {
-  interaction.classList.toggle('jump')
+  interaction.classList.toggle("jump");
+}
+
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+
+let frontend = document.querySelector("a:nth-of-type(1)");
+
+frontend.addEventListener("click", highlightHandler);
+frontend.addEventListener("animationend", highlightHandler);
+
+function highlightHandler() {
+  frontend.classList.toggle("frontend");
+}
+
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+
+let design = document.querySelector("a:nth-of-type(2)");
+
+design.addEventListener("click", scaleHandler);
+design.addEventListener("animationend", scaleHandler);
+
+function scaleHandler() {
+  design.classList.toggle("design");
+}
+
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+
+let and = document.querySelector("a:nth-of-type(3)");
+
+and.addEventListener("click", rotationHandler);
+and.addEventListener("animationend", rotationHandler);
+
+function rotationHandler() {
+  and.classList.toggle("and");
+}
+
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+
+let development = document.querySelector("a:nth-of-type(4)");
+
+development.addEventListener("click", fallHandler);
+development.addEventListener("animationend", fallHandler);
+
+function fallHandler() {
+  development.classList.toggle("development");
+}
+
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+
+let sprint5 = document.querySelector("a:nth-of-type(5)");
+
+sprint5.addEventListener("click", shapeHandler);
+sprint5.addEventListener("animationend", shapeHandler);
+
+function shapeHandler() {
+  sprint5.classList.toggle("sprint5");
 }
